46fe9a48d8d2a4e4dd62091f0ac5800c5592bfa5
charithw98 <it20258894@my.sliit.lk>
2021-10-04 14:18:29 +0530

    initial commit

---
+++ app/src/main/java/unityandroid/example/lab1/MainActivity.java
@@ -0,0 +1,15 @@
+package unityandroid.example.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
